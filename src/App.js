3ed22546041ff1f4989825430231bd7961585e6d
parth <parth.natu01@gmail.com>
2021-05-13 14:03:05 +0530

    separation of concerns

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 import './grid/grid'
 import Grid from './grid/grid';
 
+
 function App() {
+  const coins = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
+  const coinPositions = [];
+  for (let i = 0; i < 8; i++) {
+    coinPositions[i] = [];
+    for (let j = 0; j < 8; j++) {
+      if (i=== 0) {
+        coinPositions[i][j] = {
+          coin: coins[j],
+          coinColor: 'black',
+          isActive: false
+        };
+      }
+      else if (i=== 1) {
+        coinPositions[i][j] = {
+          coin: 'pawn',
+          coinColor: 'black',
+          isActive: false
+        };
+      }
+      else if (i=== 6) {
+        coinPositions[i][j] = {
+          coin: 'pawn',
+          coinColor: 'white',
+          isActive: false
+        };
+      }
+      else if (i=== 7) {
+        coinPositions[i][j] = {
+          coin: coins[j],
+          coinColor: 'white',
+          isActive: false
+        };
+      }
+      else {
+        coinPositions[i][j] = {
+          coin: '',
+          coinColor: '',
+          isActive: false
+        };
+      }
+    }
+  }
   return (
     <div className="container">
-      <Grid></Grid>
+      {/* <Grid></Grid> */}
+      <Grid coinPositions={coinPositions}></Grid>
     </div>
   );
 }
